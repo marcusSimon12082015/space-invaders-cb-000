6711207ade5a94c7b2724645af2168d7f7ed2282
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-13 08:01:44 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,42 @@
-class CrewMember {}
+class CrewMember
+{
+  constructor(position)
+  {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+  addShip(ship)
+  {
+    if(ship instanceof Spaceship)
+    {
+      this.currentShip = ship;
+    }
+  }
+  engageWarpDrive(){
+    if(this.currentShip === "Looking for a Rig"){
+      return("had no effect");
+    }else{
+      if(this.position === "Pilot"){
+        this.currentShip.warpDrive = "engaged";
+      }
+    }
+  }
+  setsInvisibility(){
+    if(this.currentShip === "Looking for a Rig"){
+      return("had no effect");
+    }else{
+      if(this.position === "Defender"){
+        this.currentShip.cloaked = true;
+      }
+    }
+  }
+  chargePhasers(){
+    if(this.currentShip === "Looking for a Rig"){
+      return("had no effect");
+    }else{
+      if(this.position === "Gunner"){
+        this.currentShip.phasersCharge = "charged";
+      }
+    }
+  }
+}
--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,18 @@
-class Spaceship {}
+class Spaceship
+{
+  constructor(name,crewMembers,phasers,shields)
+  {
+    this.name = name;
+    this.crew = crewMembers;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.docked = crewMembers.length > 0 ? false : true;
+    this.phasersCharge = "uncharged";
+    for(var i = 0; i < this.crew.length; i++)
+    {
+      this.crew[i].addShip(this);
+    }
+  }
+}
